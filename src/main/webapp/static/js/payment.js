5cfe5d3e46845b61c92fd0a06bed8ca4013f72bf
barna <barna.m42@gmail.com>
2021-12-14 18:25:24 +0100

    ADD: elements that will have event listeners or added to it

---
+++ src/main/webapp/static/js/payment.js
@@ -0,0 +1,4 @@
+const paypalElement = document.getElementById("paypal-img");
+const creditCardElement = document.getElementById("credit-card");
+const content = document.getElementById("payment-content");
+
