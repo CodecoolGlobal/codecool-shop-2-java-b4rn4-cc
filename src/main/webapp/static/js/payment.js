501225519c20b86d327401570de49f217f163cab
barna <barna.m42@gmail.com>
2021-12-14 18:31:12 +0100

    ADD: function which builds the card payment fields

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -46,3 +46,53 @@ function buildPaypalPayment() {
     // todo add event listener to build other payment option
 }
 
+function buildCardPayment() {
+    content.innerHTML = "";
+    let emailDiv = document.createElement("div");
+    let cardNumberDiv = document.createElement("div");
+    let cardHolderDiv = document.createElement("div");
+    let CCVDiv = document.createElement("div");
+    let expireDiv = document.createElement("div");
+    let payButton = document.createElement("button");
+    let otherPaymentDiv = document.createElement("div");
+
+    otherPaymentDiv.addEventListener("click", getPaypalPayment);
+
+    let emailField = document.createElement("input");
+    let cardNumberField = document.createElement("input");
+    let holderField = document.createElement("input");
+    let CCVField = document.createElement("input");
+    let expireField = document.createElement("input");
+
+    emailField.id = "email-field";
+    emailField.autocomplete = "off";
+    emailField.required = true;
+    emailField.placeholder = "Email";
+    emailField.type = "email";
+    emailDiv.append(emailField);
+
+    cardNumberField.required = true;
+    cardNumberField.placeholder = "XXXXXXX-XXXXXXXX";
+    cardNumberField.autocomplete = "off";
+    cardNumberDiv.append(cardNumberField);
+
+    holderField.required = true;
+    holderField.placeholder = "MasterCard";
+    holderField.autocomplete = "off";
+    cardHolderDiv.append(holderField);
+
+    CCVField.placeholder = "123";
+    CCVField.size = 3;
+    CCVField.required = true;
+    CCVDiv.append(CCVField);
+
+    expireField.required = true;
+    expireField.placeholder = "01/26";
+    expireDiv.append(expireField);
+
+    payButton.innerText = "Pay";
+
+    otherPaymentDiv.innerText = "Pay with PayPal";
+
+    content.append(emailDiv, cardHolderDiv, cardNumberDiv, expireDiv, CCVDiv, payButton, otherPaymentDiv);
+}
