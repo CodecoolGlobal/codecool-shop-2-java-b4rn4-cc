79aae380a3fad9f297542510d7284332c2f014f9
barna <barna.m42@gmail.com>
2021-12-15 11:51:07 +0100

    ADD: post method for sending email and todos

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.emailSender.EmailSender;
 import com.codecool.shop.service.CartService;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -14,8 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet("/payment")
 public class PaymentController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EmailSender sender = new EmailSender();
+        String emailAddress = ""; // todo get the email from the checkout
+        String emailContent = ""; // todo create the html from the cartService
+        sender.sendEmail(emailAddress, emailContent);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         CartDao cartDataStore = CartDaoMem.getInstance();
