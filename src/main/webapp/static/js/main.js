384e36670a533686257124de2f75a94218614a16
barna <barna.m42@gmail.com>
2021-12-16 09:57:41 +0100

    REFACTOR: delete unnecessary comments

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,13 +1,3 @@
-// const categorySelect = document.getElementById("sort-category");
-// const supplierSelect = document.getElementById("sort-supplier");
-//
-// categorySelect.addEventListener("change", handleSelection);
-//
-// function handleSelection(e) {
-//     const value = e.target.value;
-//     console.log(value);
-// }
-
 function addButton() {
     const addButtonList = document.getElementsByClassName("add-button");
     for (const addButton of addButtonList){
