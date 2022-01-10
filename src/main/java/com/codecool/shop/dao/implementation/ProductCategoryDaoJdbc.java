a2eaf495b704395a12c5c7fb13b04b64f926ce57
barna <barna.m42@gmail.com>
2022-01-10 19:12:08 +0100

    ADD: method to get all product categories from database

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -0,0 +1,59 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+
+    DataSource dataSource;
+
+    public ProductCategoryDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        try (Connection con = dataSource.getConnection()) {
+            String query = "SELECT id, name FROM category";
+            PreparedStatement st = con.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+
+            List<ProductCategory> categories = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+
+                ProductCategory category = new ProductCategory(name, "Hardware", "");
+                category.setId(id);
+
+                categories.add(category);
+            }
+            return categories;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
