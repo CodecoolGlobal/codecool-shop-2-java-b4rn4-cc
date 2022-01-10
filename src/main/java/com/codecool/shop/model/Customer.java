4af8792a86b10a2312249070357077d6c1fd5be2
BihariBalazs <bihari.b16@gmail.com>
2022-01-10 20:31:21 +0100

    ADD: create new constructor and fields

--- src/main/java/com/codecool/shop/model/Customer.java
+++ src/main/java/com/codecool/shop/model/Customer.java
@@ -1,12 +1,25 @@
 package com.codecool.shop.model;
 
 public class Customer {
+    private int id;
     private String name;
     private String email;
+    private String password;
     private String address;
     private String city;
     private String state;
     private String zipCode;
+
+    public Customer(int id, String name, String email, String password, String address, String city, String state, String zipCode) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
 
     public Customer(String name, String email, String address, String city, String state, String zipCode) {
         this.name = name;
