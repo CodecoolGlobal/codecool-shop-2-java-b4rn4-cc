c0cfbe6112220ed70a7a015efce27dfdb895c325
barna <barna.m42@gmail.com>
2022-01-10 19:03:02 +0100

    ADD: method to get all suppliers from database

---
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -0,0 +1,66 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupplierDaoJdbc implements SupplierDao {
+
+    DataSource dataSource;
+
+    public SupplierDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void add(Supplier supplier) {
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        try (Connection con = dataSource.getConnection()) {
+            String query = "SELECT id, name FROM supplier";
+            PreparedStatement st = con.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+
+            List<Supplier> suppliers = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+
+                Supplier supplier = new Supplier(name, "");
+                supplier.setId(id);
+
+                suppliers.add(supplier);
+            }
+            return suppliers;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Supplier> getBy(List<Product> products) {
+        return null;
+    }
+}
