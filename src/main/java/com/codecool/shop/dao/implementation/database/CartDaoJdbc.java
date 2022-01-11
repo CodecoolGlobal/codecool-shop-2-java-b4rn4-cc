ae70c03bc2d7ba2134946439f186a425ad288f03
BihariBalazs <bihari.b16@gmail.com>
2022-01-11 13:56:07 +0100

    ADD: create CartDaoJdbc

---
+++ src/main/java/com/codecool/shop/dao/implementation/database/CartDaoJdbc.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao.implementation.database;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
+public class CartDaoJdbc implements CartDao {
+
+    @Override
+    public void add(Cart cart) {
+
+    }
+
+    @Override
+    public void addToCart(Product product) {
+
+    }
+
+    @Override
+    public void deleteFromCart(int index) {
+
+    }
+
+    @Override
+    public List<Product> reviewCart() {
+        return null;
+    }
+}
