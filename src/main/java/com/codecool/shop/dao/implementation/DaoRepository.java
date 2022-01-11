6e5c3894a60bd32ea48896466038dd7011777109
BihariBalazs <bihari.b16@gmail.com>
2022-01-11 15:50:26 +0100

    ADD: create getCartDao

--- src/main/java/com/codecool/shop/dao/implementation/DaoRepository.java
+++ src/main/java/com/codecool/shop/dao/implementation/DaoRepository.java
@@ -2,10 +2,12 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.config.DatabaseConfig;
 import com.codecool.shop.config.Initializer;
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.database.DatabaseManager;
+import com.codecool.shop.dao.implementation.memory.CartDaoMem;
 import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
 import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
@@ -50,6 +52,16 @@ public class DaoRepository {
             return SupplierDaoMem.getInstance();
         } else if (conProp.getProperty("dao").equals("jdbc")) {
             return databaseManager.getSupplierDao();
+        } else {
+            return null;
+        }
+    }
+
+    public CartDao getCartDao() {
+        if (conProp.getProperty("dao").equals("memory")) {
+            return CartDaoMem.getInstance();
+        } else if (conProp.getProperty("dao").equals("jdbc")) {
+            return databaseManager.getCartDao();
         } else {
             return null;
         }
