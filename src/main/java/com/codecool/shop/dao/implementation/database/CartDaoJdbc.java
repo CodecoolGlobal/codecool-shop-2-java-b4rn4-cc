0ab7ddf51d1027487c50592f36a8b86978994aec
BihariBalazs <bihari.b16@gmail.com>
2022-01-11 14:10:37 +0100

    ADD: add method completed in CartDaoJdbc

--- src/main/java/com/codecool/shop/dao/implementation/database/CartDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/CartDaoJdbc.java
@@ -4,13 +4,32 @@ import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Product;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class CartDaoJdbc implements CartDao {
+    private DataSource dataSource;
+
+    public CartDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public void add(Cart cart) {
+        try(Connection con = dataSource.getConnection()){
+            String query = "INSERT INTO cart (user_id, payed) VALUES (?, ?)";
+            PreparedStatement st = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            st.setInt(1, cart.getUserId());
+            st.setBoolean(2, cart.isPayed());
 
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+            cart.setId(rs.getInt(1));
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
