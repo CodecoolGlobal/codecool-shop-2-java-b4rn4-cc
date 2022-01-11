fe21748020b4d6d86c050a6d9bf5c4710e25d8b5
barna <barna.m42@gmail.com>
2022-01-11 10:31:23 +0100

    ADD: method to retrieve the correct supplierDao

--- src/main/java/com/codecool/shop/dao/implementation/DaoRepository.java
+++ src/main/java/com/codecool/shop/dao/implementation/DaoRepository.java
@@ -4,9 +4,11 @@ import com.codecool.shop.config.DatabaseConfig;
 import com.codecool.shop.config.Initializer;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.database.DatabaseManager;
 import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
+import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
 
 import java.util.Properties;
 
@@ -30,6 +32,16 @@ public class DaoRepository {
             return ProductCategoryDaoMem.getInstance();
         } else if (conProp.getProperty("dao").equals("jdbc")) {
             return databaseManager.getProductCategoryDao();
+        } else {
+            return null;
+        }
+    }
+
+    public SupplierDao getSupplierDao() {
+        if (conProp.getProperty("dao").equals("memory")) {
+            return SupplierDaoMem.getInstance();
+        } else if (conProp.getProperty("dao").equals("jdbc")) {
+            return databaseManager.getSupplierDao();
         } else {
             return null;
         }
