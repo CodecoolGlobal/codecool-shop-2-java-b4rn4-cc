0e3b211afe44e408e067bac03bf9144845ed15ae
BihariBalazs <bihari.b16@gmail.com>
2022-01-12 14:47:26 +0100

    ADD: do get and do post methods

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -1,9 +1,12 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.CustomerDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.DaoRepository;
+import com.codecool.shop.model.Customer;
+import com.codecool.shop.service.CustomerService;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
@@ -22,10 +25,31 @@ import java.io.IOException;
 public class RegistrationController extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("registration.html", context, resp.getWriter());
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        DaoRepository daoRepository = DaoRepository.getInstance();
+        Customer customer = new Customer(request.getParameter("name"), request.getParameter("email"), request.getParameter("pw"));
+        if(request.getParameter("address") != null){
+            customer.setAddress(request.getParameter("address"));
+        }
+        if(request.getParameter("city") != null){
+            customer.setCity(request.getParameter("city"));
+        }
+        if(request.getParameter("state") != null){
+            customer.setState(request.getParameter("state"));
+        }
+        if(request.getParameter("zip")!= null){
+            customer.setZipCode(request.getParameter("zip"));
+        }
+        CustomerDao customerDao = daoRepository.getCustomerDaoJdbc();
+        CustomerService customerService = new CustomerService(customerDao);
+        customerService.registration(customer);
     }
 }
