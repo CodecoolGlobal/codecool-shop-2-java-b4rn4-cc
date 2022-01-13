8fbf43eb419b7734a5e8c5b5a56801cbabc41011
barna <barna.m42@gmail.com>
2022-01-13 15:10:57 +0100

    REFACTOR: method where the configuration is set up

--- src/main/java/com/codecool/shop/config/DatabaseConfig.java
+++ src/main/java/com/codecool/shop/config/DatabaseConfig.java
@@ -3,7 +3,6 @@ package com.codecool.shop.config;
 import com.codecool.shop.service.ErrorLogging;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
@@ -12,7 +11,7 @@ public class DatabaseConfig {
 
     private static Properties conProps;
 
-    public static Properties setupApplication() {
+    public static void setupApplication() {
         String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
         String conConfigPath = rootPath + "connection.properties";
 
@@ -23,8 +22,6 @@ public class DatabaseConfig {
             e.printStackTrace();
             ErrorLogging.log(e);
         }
-
-        return conProps;
     }
 
     public static Properties getConProps() {
